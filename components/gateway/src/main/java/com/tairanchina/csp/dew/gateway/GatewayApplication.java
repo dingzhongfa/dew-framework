3372234f8e4b283d12c7fc1d200f928d8fbb3583
gudaoxuri <i@sunisle.org>
2017-06-14 13:42:10 +0800

    Merge Gateway component.

--- components/gateway/src/main/java/com/tairanchina/csp/dew/gateway/GatewayApplication.java
+++ components/gateway/src/main/java/com/tairanchina/csp/dew/gateway/GatewayApplication.java
@@ -1,14 +1,16 @@
 package com.tairanchina.csp.dew.gateway;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.tairanchina.csp.dew.core.Dew;
+import com.tairanchina.csp.dew.core.DewCloudApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.netflix.zuul.filters.discovery.PatternServiceRouteMapper;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
 @EnableZuulProxy
-@SpringBootApplication
-public class GatewayApplication {
+@ComponentScan(basePackageClasses = {Dew.class, GatewayApplication.class})
+public class GatewayApplication extends DewCloudApplication {
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(GatewayApplication.class).web(true).run(args);

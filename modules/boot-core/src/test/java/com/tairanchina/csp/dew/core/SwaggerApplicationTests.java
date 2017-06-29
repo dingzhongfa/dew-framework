e84edcaa69b2bb9874430f1eb7f1d0cbba361432
有毒的菜鸟 <bxwclyxiaowei@126.com>
2017-06-29 09:19:26 +0800

    add plugins and TestClass for swagger doc

---
+++ modules/boot-core/src/test/java/com/tairanchina/csp/dew/core/SwaggerApplicationTests.java
@@ -0,0 +1,79 @@
+package com.tairanchina.csp.dew.core;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.alibaba.fastjson.JSON;
+
+import io.github.robwin.markup.builder.MarkupLanguage;
+import io.github.robwin.swagger2markup.GroupBy;
+import io.github.robwin.swagger2markup.Swagger2MarkupConverter;
+import springfox.documentation.staticdocs.SwaggerResultHandler;
+import springfox.documentation.swagger2.web.Swagger2Controller;
+
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs(outputDir = "target/generated-snippets")
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SwaggerApplicationTests {
+
+	 private String snippetDir = "target/generated-snippets";
+	 private String outputDir = "target/asciidoc";
+	 
+	 @Autowired
+	 private MockMvc mockMvc;
+	 
+	 @After
+	    public void Test() throws Exception{
+	        // 得到swagger.json,写入outputDir目录中
+	        mockMvc.perform(get(Swagger2Controller.DEFAULT_URL).accept(MediaType.APPLICATION_JSON))
+	                .andDo(SwaggerResultHandler.outputDirectory(outputDir).build())
+	                .andExpect(status().isOk())
+	                .andReturn();
+
+	        // 读取上一步生成的swagger.json转成asciiDoc,写入到outputDir
+	        // 这个outputDir必须和插件里面<generated></generated>标签配置一致
+	        Swagger2MarkupConverter.from(outputDir + "/swagger.json")
+	                .withPathsGroupedBy(GroupBy.TAGS)// 按tag排序
+	                .withMarkupLanguage(MarkupLanguage.ASCIIDOC)// 格式
+	                .withExamples(snippetDir)
+	                .build()
+	                .intoFolder(outputDir);// 输出
+	    }
+	@Test
+	public void contextLoads() throws Exception {
+		 mockMvc.perform(get("/student1").param("name", "xxx")
+	                .accept(MediaType.APPLICATION_JSON))
+	                .andExpect(status().isOk())
+	                .andDo(MockMvcRestDocumentation.document("getStudent", preprocessResponse(prettyPrint())));
+
+//	        Student student = new Student();
+//	        student.setName("xxx");
+//	        student.setAge(23);
+//	        student.setAddress("湖北麻城");
+//	        student.setCls("二年级");
+//	        student.setSex("男");
+
+	        mockMvc.perform(post("/addStudent1").contentType(MediaType.APPLICATION_JSON) //addStudent1 为请求地址，文档中接口名
+	                .content(JSON.toJSONString("？")) // ？ 放置对象参数
+	                .accept(MediaType.APPLICATION_JSON))
+	                .andExpect(status().is2xxSuccessful())
+	                .andDo(MockMvcRestDocumentation.document("addStudent", preprocessResponse(prettyPrint()))); //addStudent为文档中接口方法名
+	}
+
+}

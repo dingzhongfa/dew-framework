6123425cea6b5a1a075939ad022fb04f48740437
有毒的菜鸟 <bxwclyxiaowei@126.com>
2017-06-29 14:10:36 +0800

    change SwaggerTestClass

--- modules/boot-core/src/test/java/com/tairanchina/csp/dew/core/SwaggerApplicationTests.java
+++ modules/boot-core/src/test/java/com/tairanchina/csp/dew/core/SwaggerApplicationTests.java
@@ -1,5 +1,8 @@
 package com.tairanchina.csp.dew.core;
 
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,23 +11,14 @@ import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDoc
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-
-import com.alibaba.fastjson.JSON;
 
 import io.github.robwin.markup.builder.MarkupLanguage;
 import io.github.robwin.swagger2markup.GroupBy;
 import io.github.robwin.swagger2markup.Swagger2MarkupConverter;
 import springfox.documentation.staticdocs.SwaggerResultHandler;
 import springfox.documentation.swagger2.web.Swagger2Controller;
-
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 
 @AutoConfigureMockMvc
 @AutoConfigureRestDocs(outputDir = "target/generated-snippets")
@@ -55,25 +49,30 @@ public class SwaggerApplicationTests {
 	                .build()
 	                .intoFolder(outputDir);// 输出
 	    }
+	 
+	 /**
+	  * 如果不需要每个接口的返回结果，那直接运行测试类，就行，不需要走每个测试。
+	  * @throws Exception
+	  */
 	@Test
 	public void contextLoads() throws Exception {
-		 mockMvc.perform(get("/student1").param("name", "xxx")
-	                .accept(MediaType.APPLICATION_JSON))
-	                .andExpect(status().isOk())
-	                .andDo(MockMvcRestDocumentation.document("getStudent", preprocessResponse(prettyPrint())));
-
-//	        Student student = new Student();
-//	        student.setName("xxx");
-//	        student.setAge(23);
-//	        student.setAddress("湖北麻城");
-//	        student.setCls("二年级");
-//	        student.setSex("男");
-
-	        mockMvc.perform(post("/addStudent1").contentType(MediaType.APPLICATION_JSON) //addStudent1 为请求地址，文档中接口名
-	                .content(JSON.toJSONString("？")) // ？ 放置对象参数
-	                .accept(MediaType.APPLICATION_JSON))
-	                .andExpect(status().is2xxSuccessful())
-	                .andDo(MockMvcRestDocumentation.document("addStudent", preprocessResponse(prettyPrint()))); //addStudent为文档中接口方法名
+//		 mockMvc.perform(get("/student1").param("name", "xxx")
+//	                .accept(MediaType.APPLICATION_JSON))
+//	                .andExpect(status().isOk())
+//	                .andDo(MockMvcRestDocumentation.document("getStudent", preprocessResponse(prettyPrint())));
+//
+////	        Student student = new Student();
+////	        student.setName("xxx");
+////	        student.setAge(23);
+////	        student.setAddress("湖北麻城");
+////	        student.setCls("二年级");
+////	        student.setSex("男");
+//
+//	        mockMvc.perform(post("/addStudent1").contentType(MediaType.APPLICATION_JSON) //addStudent1 为请求地址，文档中接口名
+//	                .content(JSON.toJSONString("？")) // ？ 放置对象参数
+//	                .accept(MediaType.APPLICATION_JSON))
+//	                .andExpect(status().is2xxSuccessful())
+//	                .andDo(MockMvcRestDocumentation.document("addStudent", preprocessResponse(prettyPrint()))); //addStudent为文档中接口方法名
 	}
 
 }

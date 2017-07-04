dab9ae3ded794b7e241a753bd653b294f6f6815c
gudaoxuri <i@sunisle.org>
2017-07-04 17:30:31 +0800

    Supported RabbitMQ . close #12

--- modules/cluster-rabbit/src/main/java/com/tairanchina/csp/dew/core/cluster/spi/rabbit/RabbitAdapter.java
+++ modules/cluster-rabbit/src/main/java/com/tairanchina/csp/dew/core/cluster/spi/rabbit/RabbitAdapter.java
@@ -1,6 +1,5 @@
 package com.tairanchina.csp.dew.core.cluster.spi.rabbit;
 
-import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.rabbit.connection.Connection;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,10 +12,6 @@ public class RabbitAdapter {
 
     @Autowired
     private RabbitTemplate rabbitTemplate;
-
-    AmqpTemplate getRabbitTemplate() {
-        return rabbitTemplate;
-    }
 
     Connection getConnection() {
         return rabbitTemplate.getConnectionFactory().createConnection();

bd8328e683aa6f69edfc4ec959d1c109395c61f8
gudaoxuri <i@sunisle.org>
2017-09-13 20:58:13 +0800

    Writing user manual & perfect the examples. #30 #4 #33

--- examples/hystrix-example/src/main/java/com/tairanchina/csp/dew/example/hystrix/HystrixExampleApplication.java
+++ examples/hystrix-example/src/main/java/com/tairanchina/csp/dew/example/hystrix/HystrixExampleApplication.java
@@ -1,11 +1,14 @@
 package com.tairanchina.csp.dew.example.hystrix;
 
+import com.tairanchina.csp.dew.core.Dew;
 import com.tairanchina.csp.dew.core.DewCloudApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * 工程启动类
  */
+@ComponentScan(basePackageClasses = {Dew.class, HystrixExampleApplication.class})
 public class HystrixExampleApplication extends DewCloudApplication {
 
     public static void main(String[] args) {

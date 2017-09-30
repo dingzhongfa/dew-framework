2629359e8457400764e4109797bbaf707b96038d
gudaoxuri <i@sunisle.org>
2017-09-30 16:39:37 +0800

    Release 1.0.0-RC

--- examples/web-example/src/test/java/com/tairanchina/csp/dew/example/DocTest.java
+++ examples/web-example/src/test/java/com/tairanchina/csp/dew/example/DocTest.java
@@ -1,18 +1,13 @@
 package com.tairanchina.csp.dew.example;
 
-import com.tairanchina.csp.dew.core.Dew;
 import com.tairanchina.csp.dew.example.web.WebExampleApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootApplication
 @SpringBootTest(classes = WebExampleApplication.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-@ComponentScan(basePackageClasses = {Dew.class, WebExampleApplication.class})
 public class DocTest {
 
     @Test

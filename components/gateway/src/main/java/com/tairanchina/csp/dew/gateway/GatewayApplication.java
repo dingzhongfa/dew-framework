2d9d7bab94c67c63f549693e3b85e22068130a40
Ding <71964899@qq.com>
2017-10-18 10:36:45 +0800

    移除CloudApplication多余注解

--- components/gateway/src/main/java/com/tairanchina/csp/dew/gateway/GatewayApplication.java
+++ components/gateway/src/main/java/com/tairanchina/csp/dew/gateway/GatewayApplication.java
@@ -1,16 +1,13 @@
 package com.tairanchina.csp.dew.gateway;
 
-import com.tairanchina.csp.dew.Dew;
 import com.tairanchina.csp.dew.core.DewCloudApplication;
 import com.tairanchina.csp.dew.gateway.auth.LoggingFilter;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.netflix.zuul.filters.discovery.PatternServiceRouteMapper;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 
 @EnableZuulProxy
-@ComponentScan(basePackageClasses = {Dew.class, GatewayApplication.class})
 public class GatewayApplication extends DewCloudApplication {
 
     public static void main(String[] args) {

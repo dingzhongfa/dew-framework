1aa6afd0403700141d53a8c686f1425b229aac8a
gudaoxuri <i@sunisle.org>
2017-11-06 14:59:18 +0800

    #53 增加Resp模型对服务降级的处理

--- modules/boot-starter/src/main/java/com/tairanchina/csp/dew/core/controller/ErrorController.java
+++ modules/boot-starter/src/main/java/com/tairanchina/csp/dew/core/controller/ErrorController.java
@@ -35,7 +35,7 @@ public class ErrorController extends AbstractErrorController {
 
     private static final Logger logger = LoggerFactory.getLogger(ErrorController.class);
 
-    private static final int FALL_BACK_STATUS = 1000;
+    private static final int FALL_BACK_STATUS = 555;
     private static final Pattern MESSAGE_CHECK = Pattern.compile("^\\{\"code\":\"\\w*\",\"message\":\".*\",\"customHttpCode\":.*}$");
 
     private static final String SPECIAL_ERROR_FLAG = "org.springframework.boot.autoconfigure.web.DefaultErrorAttributes.ERROR";

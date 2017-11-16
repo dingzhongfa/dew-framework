65f952f0d86f4c3d1f75fca5b41cc9122187ad67
Ding <71964899@qq.com>
2017-11-16 14:07:40 +0800

    实现TPS查询接口 #72

--- modules/boot-starter/src/main/java/com/tairanchina/csp/dew/core/metric/DewMetrics.java
+++ modules/boot-starter/src/main/java/com/tairanchina/csp/dew/core/metric/DewMetrics.java
@@ -6,6 +6,7 @@ import org.springframework.boot.actuate.endpoint.PublicMetrics;
 import org.springframework.boot.actuate.metrics.Metric;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
 import java.time.Instant;
 import java.util.*;
 
@@ -24,7 +25,9 @@ public class DewMetrics implements PublicMetrics {
         long divid = Instant.now().minusSeconds(INTERVAL_SEC).toEpochMilli();
         List<Metric<?>> metricList = new ArrayList<>();
         List<Integer> totalList = new ArrayList<>();
-        List<Integer> averageList = new ArrayList<>();
+        // url级平均响应时间->url级个数
+        Map<Double, Integer> averageMap = new HashMap<>();
+        BigDecimal intervalSec = new BigDecimal(INTERVAL_SEC);
         DewFilter.RECORD_MAP.forEach((key, value) -> {
             long urlSum = 0;
             List<Integer> validList = new ArrayList<>();
@@ -39,27 +42,25 @@ public class DewMetrics implements PublicMetrics {
             Arrays.sort(urlTimeArr);
             int nityPec = (int) urlTimeArr[(int) (validList.size() * 0.9)];
             int max = (int) urlTimeArr[(validList.size() - 1)];
-            int average = (int) (urlSum / validList.size());
-            metricList.add(new Metric<>("dew.response.average." + key, average));
+            double average = (double)urlSum / validList.size();
+            metricList.add(new Metric<>("dew.response.average." + key, BigDecimal.valueOf(average).setScale(2,BigDecimal.ROUND_HALF_UP)));
             metricList.add(new Metric<>("dew.response.90percent." + key, nityPec));
             metricList.add(new Metric<>("dew.response.max." + key, max));
-            metricList.add(new Metric<>("dew.response.tps." + key, validList.size() / INTERVAL_SEC));
-            averageList.add(average);
+            BigDecimal times = new BigDecimal(validList.size());
+            metricList.add(new Metric<>("dew.response.tps." + key, times.divide(intervalSec,2,BigDecimal.ROUND_HALF_UP)));
+            averageMap.put(average, validList.size());
         });
-        if (averageList.size() != 0) {
-            int totalSum = 0;
-            for (int ave : averageList) {
-                totalSum += ave;
-            }
-            metricList.add(new Metric<>("dew.response.average", totalSum / averageList.size()));
+        double totalAverage = 0;
+        for (Map.Entry<Double,Integer> entry: averageMap.entrySet()){
+            totalAverage+= entry.getKey()*((double)entry.getValue()/totalList.size());
         }
-        if (totalList.size() != 0) {
-            Object[] totalArr = totalList.toArray();
-            Arrays.sort(totalArr);
-            metricList.add(new Metric<>("dew.response.90perent", (Integer) totalArr[(int) (totalList.size() * 0.9)]));
-            metricList.add(new Metric<>("dew.response.max", (Integer) totalArr[totalList.size() - 1]));
-            metricList.add(new Metric<>("dew.response.tps", totalList.size() / INTERVAL_SEC));
-        }
+        Object[] totalArr = totalList.toArray();
+        Arrays.sort(totalArr);
+        metricList.add(new Metric<>("dew.response.average",BigDecimal.valueOf(totalAverage).setScale(2,BigDecimal.ROUND_HALF_UP)));
+        metricList.add(new Metric<>("dew.response.90perent", (Integer) totalArr[(int) (totalList.size() * 0.9)]));
+        metricList.add(new Metric<>("dew.response.max", (Integer) totalArr[totalList.size() - 1]));
+        BigDecimal totalTimes = new BigDecimal(totalList.size());
+        metricList.add(new Metric<>("dew.response.tps", totalTimes.divide(intervalSec,2,BigDecimal.ROUND_HALF_UP)));
         return metricList;
     }
 

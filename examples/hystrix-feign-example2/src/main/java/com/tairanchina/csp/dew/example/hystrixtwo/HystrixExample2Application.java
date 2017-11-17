e5ca020b6b8c38401fc6ee50949a52b701921d13
Ding <71964899@qq.com>
2017-11-17 11:34:43 +0800

    添加测试项目 #73

---
+++ examples/hystrix-feign-example2/src/main/java/com/tairanchina/csp/dew/example/hystrixtwo/HystrixExample2Application.java
@@ -0,0 +1,18 @@
+package com.tairanchina.csp.dew.example.hystrixtwo;
+
+import com.tairanchina.csp.dew.core.DewCloudApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+/**
+ * desription:
+ * Created by ding on 2017/11/17.
+ */
+@EnableFeignClients
+public class HystrixExample2Application extends DewCloudApplication {
+
+    public static void main(String[] args) {
+        System.out.println("started");
+        new SpringApplicationBuilder(HystrixExample2Application.class).build(args);
+    }
+}

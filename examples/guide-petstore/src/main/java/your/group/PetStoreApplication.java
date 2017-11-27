421c48c134517fd7498137e2983910129ae595b8
Ding <71964899@qq.com>
2017-11-27 18:04:50 +0800

    review

--- examples/guide-petstore/src/main/java/your/group/PetStoreApplication.java
+++ examples/guide-petstore/src/main/java/your/group/PetStoreApplication.java
@@ -2,11 +2,13 @@ package your.group;
 
 import com.tairanchina.csp.dew.core.autoconfigure.DewBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * 工程启动类
  */
 @DewBootApplication
+@ComponentScan(basePackageClasses = PetStoreApplication.class)
 public class PetStoreApplication {
 
     public static void main(String[] args) {

b2657d327d810d868af59a8923ae871db1d83a79
gudaoxuri <i@sunisle.org>
2017-11-27 11:26:49 +0800

    增加注解启用Dew功能 #80

--- examples/jdbc-example/src/main/java/com/tairanchina/csp/dew/example/jdbc/JDBCExampleApplication.java
+++ examples/jdbc-example/src/main/java/com/tairanchina/csp/dew/example/jdbc/JDBCExampleApplication.java
@@ -1,13 +1,12 @@
 package com.tairanchina.csp.dew.example.jdbc;
 
-import com.tairanchina.csp.dew.core.autoconfigure.DewBootApplication;
+import com.tairanchina.csp.dew.core.DewBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
 /**
  * 工程启动类
  */
-@DewBootApplication
-public class JDBCExampleApplication {
+public class JDBCExampleApplication extends DewBootApplication {
 
     public static void main(String[] args) throws InterruptedException {
         new SpringApplicationBuilder(JDBCExampleApplication.class).run(args);

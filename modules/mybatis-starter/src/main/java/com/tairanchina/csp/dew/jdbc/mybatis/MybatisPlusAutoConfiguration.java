5a98f221613ce5ba57eb0ed6dc670e117272307b
Ding <71964899@qq.com>
2017-12-29 17:05:50 +0800

    demo版及example项目提交 #87

--- modules/mybatis-starter/src/main/java/com/tairanchina/csp/dew/jdbc/mybatis/MybatisPlusAutoConfiguration.java
+++ modules/mybatis-starter/src/main/java/com/tairanchina/csp/dew/jdbc/mybatis/MybatisPlusAutoConfiguration.java
@@ -21,7 +21,10 @@ import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.condition.*;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
@@ -56,8 +59,8 @@ public class MybatisPlusAutoConfiguration {
         this.properties = properties;
         this.interceptors = interceptorsProvider.getIfAvailable();
         this.resourceLoader = resourceLoader;
-        this.databaseIdProvider =  databaseIdProvider.getIfAvailable();
-        this.configurationCustomizers =  configurationCustomizersProvider.getIfAvailable();
+        this.databaseIdProvider = databaseIdProvider.getIfAvailable();
+        this.configurationCustomizers = configurationCustomizersProvider.getIfAvailable();
     }
 
     @PostConstruct
@@ -83,6 +86,7 @@ public class MybatisPlusAutoConfiguration {
 
 
     @Bean(name = "shardingSqlSessionFactory")
+    @ConditionalOnBean(ShardingEnvironmentAware.class)
     public SqlSessionFactory shardingSqlSessionFactory(@Autowired ShardingEnvironmentAware shardingEnvironmentAware) throws Exception {
         MybatisSqlSessionFactoryBean factory = new MybatisSqlSessionFactoryBean();
         factory.setDataSource(shardingEnvironmentAware.dataSource());
@@ -90,24 +94,23 @@ public class MybatisPlusAutoConfiguration {
     }
 
     @Bean(name = "shardingSqlSessionTemplate")
+    @ConditionalOnBean(ShardingEnvironmentAware.class)
     public SqlSessionTemplate shardingSqlSessionTemplate(@Qualifier("shardingSqlSessionFactory") SqlSessionFactory sqlSessionFactory) throws Exception {
         ExecutorType executorType = this.properties.getExecutorType();
         return executorType != null ? new SqlSessionTemplate(sqlSessionFactory, executorType) : new SqlSessionTemplate(sqlSessionFactory);
     }
 
     @Bean(name = "secondSqlSessionFactory")
-//    @ConditionalOnExpression("'dew.mybatis.second'=='true'")
-    @ConditionalOnProperty(prefix = "dew.mybatis",name = "second",havingValue = "true")
+    @ConditionalOnProperty(prefix = "dew.mybatis", name = "second", havingValue = "true")
     public SqlSessionFactory secondSqlSessionFactory(@Autowired ApplicationContext applicationContext) throws Exception {
         MybatisSqlSessionFactoryBean factory = new MybatisSqlSessionFactoryBean();
-        DataSource dataSource = ((DewDS)applicationContext.getBean(DewMapperScannerRegister.secondPrefix+"DS")).jdbc().getDataSource();
+        DataSource dataSource = ((DewDS) applicationContext.getBean(DewMapperScannerRegister.secondPrefix + "DS")).jdbc().getDataSource();
         factory.setDataSource(dataSource);
         return creatSqlSessionFactory(factory);
     }
 
     @Bean(name = "secondSqlSessionTemplate")
-//    @ConditionalOnExpression("'dew.mybatis.second'=='true'")
-    @ConditionalOnProperty(prefix = "dew.mybatis",name = "second",havingValue = "true")
+    @ConditionalOnProperty(prefix = "dew.mybatis", name = "second", havingValue = "true")
     public SqlSessionTemplate secondSqlSessionTemplate(@Qualifier("secondSqlSessionFactory") SqlSessionFactory sqlSessionFactory) throws Exception {
         ExecutorType executorType = this.properties.getExecutorType();
         return executorType != null ? new SqlSessionTemplate(sqlSessionFactory, executorType) : new SqlSessionTemplate(sqlSessionFactory);

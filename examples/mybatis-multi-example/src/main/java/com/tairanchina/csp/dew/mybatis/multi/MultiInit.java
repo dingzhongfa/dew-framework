4e54ea80a5d674a149390c7d8b23f01e4bb23de9
Ding <71964899@qq.com>
2018-01-03 10:33:02 +0800

    xml更改 #87

--- examples/mybatis-multi-example/src/main/java/com/tairanchina/csp/dew/mybatis/multi/MultiInit.java
+++ examples/mybatis-multi-example/src/main/java/com/tairanchina/csp/dew/mybatis/multi/MultiInit.java
@@ -9,7 +9,6 @@ import io.shardingjdbc.transaction.constants.SoftTransactionType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.datasource.DataSourceUtils;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;

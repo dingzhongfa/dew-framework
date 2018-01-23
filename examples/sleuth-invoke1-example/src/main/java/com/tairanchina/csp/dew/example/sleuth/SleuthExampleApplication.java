ad48f07fb54078f137ae7259a4efd4bfa92b40ef
Ding <71964899@qq.com>
2018-01-23 10:19:55 +0800

    日志中解析message,动态显示property

--- examples/sleuth-invoke1-example/src/main/java/com/tairanchina/csp/dew/example/sleuth/SleuthExampleApplication.java
+++ examples/sleuth-invoke1-example/src/main/java/com/tairanchina/csp/dew/example/sleuth/SleuthExampleApplication.java
@@ -1,11 +1,14 @@
 package com.tairanchina.csp.dew.example.sleuth;
 
+import ch.qos.logback.classic.PatternLayout;
 import com.tairanchina.csp.dew.core.DewCloudApplication;
+import com.tairanchina.csp.dew.example.sleuth.logger.TestConverter;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
 public class SleuthExampleApplication extends DewCloudApplication {
 
     public static void main(String[] args) {
+        PatternLayout.defaultConverterMap.put("dew", TestConverter.class.getName());
         new SpringApplicationBuilder(SleuthExampleApplication.class).run(args);
     }
 
